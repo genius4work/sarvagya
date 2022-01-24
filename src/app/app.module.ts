29e4af859afdcf62fa9c468834a9d8c8a5f25330
Munish Sharma <munish4humanity@gmail.com>
2022-01-24 20:33:33 +0530

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HumbergerComponent } from './humberger/humberger.component';
+import { HeroComponent } from './hero/hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HumbergerComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/humberger/humberger.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-humberger',
+  templateUrl: './humberger.component.html',
+  styleUrls: ['./humberger.component.scss']
+})
+export class HumbergerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
